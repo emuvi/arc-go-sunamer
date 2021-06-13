c7448c8d405e98026b3512ee56d1ed737ba6450b
Éverton M. Vieira <everton.muvi@gmail.com>
2021-06-13 13:32:33 -0300

    prototype development

--- main.go
+++ main.go
@@ -4,10 +4,10 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"os"
 	"path"
 	"path/filepath"
+	"regexp"
 	"strconv"
 	"strings"
 	"unicode"
@@ -17,23 +17,91 @@ import (
 	"golang.org/x/text/unicode/norm"
 )
 
-var helper bool
 var subdirs bool
-var remove_accents bool
+var trim_spaces bool
+var many_spaces_to_singles bool
 var spaces_to_underscores bool
+var remove_accents bool
+var helper bool
+
+type Option struct {
+	call string
+	help string
+	plug *bool
+}
+
+var options = []Option{
+	{
+		call: "sd",
+		help: "Sunamer in the subdirectories.",
+		plug: &subdirs,
+	},
+	{
+		call: "ts",
+		help: "Trim spaces from begin and end.",
+		plug: &trim_spaces,
+	},
+	{
+		call: "ss",
+		help: "Replace many spaces to singles.",
+		plug: &many_spaces_to_singles,
+	},
+	{
+		call: "su",
+		help: "Replace spaces to underscores.",
+		plug: &spaces_to_underscores,
+	},
+	{
+		call: "ra",
+		help: "Remove accents from name.",
+		plug: &remove_accents,
+	},
+	{
+		call: "h",
+		help: "Show the usage help message.",
+		plug: &helper,
+	},
+}
+
+func main() {
+	for _, option := range options {
+		flag.BoolVar(option.plug, option.call, false, option.help)
+	}
+	flag.Usage = func() {
+		flagSet := flag.CommandLine
+		for _, option := range options {
+			flag := flagSet.Lookup(option.call)
+			fmt.Printf("-%s\n", flag.Name)
+			fmt.Printf("  %s\n", flag.Usage)
+		}
+	}
+	flag.Parse()
+	if helper {
+		fmt.Println("Usage of sunamer [OPTION]... [INPUT]...")
+		fmt.Println("Options:")
+		flag.Usage()
+		os.Exit(0)
+	}
+	args := flag.Args()
+	if len(args) == 0 {
+		fmt.Println("You must pass at least one input file.")
+		os.Exit(-1)
+	}
+	for _, input := range args {
+		sunamer(input)
+	}
+}
 
 func sunamer(origin string) {
 	info, err := os.Stat(origin)
 	if err != nil {
-		log.Println(err)
-		return
+		panic(err)
 	}
 	if info.IsDir() && subdirs {
 		fmt.Println("Folder: " + origin)
 		files, err := ioutil.ReadDir(origin)
 		if err != nil {
-			log.Println(err)
-			return
+			panic(err)
 		}
 		for _, inside := range files {
 			sunamer(path.Join(origin, inside.Name()))
@@ -42,11 +110,17 @@ func sunamer(origin string) {
 		fmt.Println("File: " + origin)
 		oldName := info.Name()
 		newName := oldName
-		if remove_accents {
-			newName = doRemoveAccents(newName)
+		if trim_spaces {
+			newName = doTrimSpaces(newName)
+		}
+		if many_spaces_to_singles {
+			newName = doManySpacesToSingles(newName)
 		}
 		if spaces_to_underscores {
 			newName = doSpacesToUnderscores(newName)
+		}
+		if remove_accents {
+			newName = doRemoveAccents(newName)
 		}
 		if newName == oldName {
 			fmt.Println("Nothing to do.")
@@ -67,48 +141,30 @@ func sunamer(origin string) {
 			fmt.Println(destiny)
 			err := os.Rename(origin, destiny)
 			if err != nil {
-				log.Println(err)
+				panic(err)
 			}
 		}
 	}
 }
 
-func main() {
-	flag.BoolVar(&helper, "h", false, "Show the usage help message.")
-	flag.BoolVar(&subdirs, "d", false, "Sunamer in the subdirectories.")
-	flag.BoolVar(&remove_accents, "ra", false, "Remove accents from name.")
-	flag.BoolVar(&spaces_to_underscores, "su", false, "Replace spaces to underscores.")
-	flag.Parse()
-	if helper {
-		fmt.Printf("Usage of sunamer [OPTION]... [INPUT]...\n")
-		flag.PrintDefaults()
-		os.Exit(0)
-	}
-	args := flag.Args()
-	if len(args) == 0 {
-		fmt.Println("You must pass at least one input file.")
-		os.Exit(-1)
-	}
-	for _, input := range args {
-		sunamer(input)
-	}
+func doTrimSpaces(name string) string {
+	return strings.TrimSpace(name)
 }
 
-func doRemoveAccents(s string) string {
+func doManySpacesToSingles(name string) string {
+	re := regexp.MustCompile(`\s+`)
+	return re.ReplaceAllString(name, " ")
+}
+
+func doSpacesToUnderscores(name string) string {
+	return strings.ReplaceAll(name, " ", "_")
+}
+
+func doRemoveAccents(name string) string {
 	transformer := transform.Chain(norm.NFD, runes.Remove(runes.In(unicode.Mn)), norm.NFC)
-	result, _, err := transform.String(transformer, s)
+	result, _, err := transform.String(transformer, name)
 	if err != nil {
-		log.Println(err)
-		return s
-	}
-	return result
-}
-
-func doSpacesToUnderscores(s string) string {
-	result := strings.TrimSpace(s)
-	result = strings.ReplaceAll(result, " ", "_")
-	for strings.Contains(result, "__") {
-		result = strings.ReplaceAll(result, "__", "_")
+		panic(err)
 	}
 	return result
 }
